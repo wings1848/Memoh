27985d356afedc931750fce82d9d8fa5b1c7807d
Acbox <acbox0328@gmail.com>
2026-05-02 17:48:16 +0800

    docs: add blog post introducing Discuss mode

--- docs/docs/.vitepress/blogs.ts
+++ docs/docs/.vitepress/blogs.ts
@@ -6,6 +6,10 @@ export const blogs = [
       {
         text: 'Introduction (Feb 2026)',
         link: '/blogs/2026-02-16.md'
+      },
+      {
+        text: '介绍Memoh的Discuss模式 - 我们如何实现一个更主动的群聊机器人 (May 2026)',
+        link: '/blogs/2026-05-02.md'
       }
     ]
   }
